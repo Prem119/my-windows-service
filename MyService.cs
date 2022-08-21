12b550a32710cb9d29a8389c136220f8d497f3b0
Premchand Thurvas <Premchand.Thurvas@microfocus.com>
2022-08-21 17:40:50 +0530

    Initial commit

---
+++ MyService.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceProcess;
+using System.Threading;
+
+namespace MyWindowsService
+{
+    public partial class MyService : ServiceBase
+    {
+        private readonly int secs = 7;
+
+        public MyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread.Sleep(1000 * secs);
+        }
+
+        protected override void OnStop()
+        {
+            Thread.Sleep(1000 * secs);
+        }
+
+        protected override void OnContinue()
+        {
+            Thread.Sleep(1000 * secs);
+        }
+
+        protected override void OnPause()
+        {
+            Thread.Sleep(1000 * secs);
+        }
+    }
+}
